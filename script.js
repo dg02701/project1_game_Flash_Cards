2474f82074775c1926927c533276566e7bfe5ee4
Darryl Grissom <GrissomDarrylE@JohnDeere>
2020-07-13 18:27:46 -0500

    working on f nextCard, added validkey init at top of invalidKeyPressed, still working on gracefully ending card deck

--- script.js
+++ script.js
@@ -2,75 +2,115 @@
 const previousKeysPressed = new Array();
 previousKeysPressed[0] = "START"
 console.log(previousKeysPressed + " GLOBAL");
-
-
-
+let counter = 0;  // GLOBAL counter for working through flashCard deck
+console.log("counter initialized " + counter);
 
 const flashCards = [
     {
         prompt:"Who said?",
-        quote:"Whether you think you can or think you can’t, you’re right.",
+        quote:"0Whether you think you can or think you can’t, you’re right.",
         author:"Henry Ford"
     },
     {
         prompt:"Who said?",
-        quote:"The pessimist sees difficulty in every opportunity. The optimist sees opportunity in every difficulty.",
+        quote:"1The pessimist sees difficulty in every opportunity. The optimist sees opportunity in every difficulty.",
         author:"Winston Churchill"
-    }];
+    },
+    {
+        prompt:"Who said?",
+        quote:"2The pessimist sees difficulty in every opportunity. The optimist sees opportunity in every difficulty.",
+        author:"Winston Churchill"
+    // },
+    // {
+    //     prompt:"Who said?",
+    //     quote:"3The pessimist sees difficulty in every opportunity. The optimist sees opportunity in every difficulty.",
+    //     author:"Winston Churchill"
+    // },
+    // {
+    //     prompt:"Who said?",
+    //     quote:"4The pessimist sees difficulty in every opportunity. The optimist sees opportunity in every difficulty.",
+    //     author:"Winston Churchill"
+    // },
+    // {
+    //     prompt:"Who said?",
+    //     quote:"5The pessimist sees difficulty in every opportunity. The optimist sees opportunity in every difficulty.",
+    //     author:"Winston Churchill"
+    // },
+    // {
+    //     prompt:"Who said?",
+    //     quote:"6The pessimist sees difficulty in every opportunity. The optimist sees opportunity in every difficulty.",
+    //     author:"Winston Churchill"
+    // }];
+    }];   // takeout when uncomment Q 3-6.
 console.log(Array.isArray(flashCards));
 console.log(flashCards[0].prompt);
 console.log(flashCards[0].quote);
 
-
-// flashCards.forEach(card => {
-
-// })
-
-function invalidKeyPressed(key, preKey){
-    if (key !== ("ArrowRight" || "KeyA" || "KeyY" || "KeyN")){
-        console.log("key argument - " + key);
-        console.log("Not a game key, // call showHelpKeys");
-        // call showHelpKeys
-    }else if ((key === "ArrowRight" && preKey[0] === "ArrowRight")) {
-        // call wantToSkip
-        console.log("// call wantToSkip");
-    }else if ((key === "ArrowRight" && preKey[0] === "KeyA")){
-         // call wantToSkip
-         console.log("// call wantToSkip");       
-    }else if ((key === "KeyA" && preKey[0] === "KeyA")){
-        // call answerAlreadyDisplayed
-        console.log("// call answerAlreadyDisplayed"); 
-    }else if ((key === "KeyA" && preKey[0] === "KeyY")){
-        // call answerShowingAndScored, will need arguement of score
-        console.log("// call answerShowingAndScored -  Y branch"); 
-    }else if ((key === "KeyA" && preKey[0] === "KeyN")){
-        // call answerShowingAndScored, will need arguement of score
-        console.log("// call answerShowingAndScored - N branch"); 
-    }else if ((key === "KeyY" && preKey[0] === "ArrowRight")){
-        // call youHaveNotSeenAnswer
-        console.log("// call youHaveNotSeenAnswer - from Y");
-    }else if ((key === "KeyY" && preKey[0] === "KeyY")){
-        // call alreadyScored
-        console.log("// call alreadyScored - Y");
-    }else if ((key === "KeyY" && preKey[0] === "KeyN")){
-        // call alreadyScored
-        console.log("// call alreadyScored - N");  
-    }else if ((key === "KeyN" && preKey[0] === "ArrowRight")){
-        // call youHaveNotSeenAnswer
-        console.log("// call youHaveNotSeenAnswer - from N branch");
-    }else if ((key === "KeyN" && preKey[0] === "KeyY")){
-        // call alreadyScored
-        console.log("// call alreadyScored - Y");
-    }else if ((key === "KeyN" && preKey[0] === "KeyN")){
-        // call alreadyScored
-        console.log("// call alreadyScored - N");  
-    }else {
-        console.log("You fell through invalidKeyPressed.  You should NOT be here!!!")
-    };
+function nextCard (){
+    console.log("in nextCard, counter starts as: = " + counter);
+    console.log("flashCards.length= " + flashCards.length);
+    let cardFront = document.querySelector("#cardFront");
+    cardFront.innerText = flashCards[counter].quote;
+    counter = counter + 1;
 };
 
-// console.log("in ELSE of f invalidKeyPressed");
-
+function invalidKeyPressed(key, preKey){
+    let validKey = false;
+    console.log("key argument - " + key);
+    console.log("prekey argument - " + preKey);
+    if (key === "ArrowRight"){
+        let validKey = true;
+    }else if (key === "KeyA"){
+        let validKey = true;
+    }else if (key === "KeyY"){
+        let validKey = true;
+    }else if (key === "KeyN"){
+        let validKey = true;
+    }else {
+        let validKey = false;
+    };
+    if (validKey === false){
+    console.log("Not a game key, // call showHelpKeys");
+    // call showHelpKeys
+    }else{
+        if ((key === "ArrowRight" && preKey[0] === "ArrowRight")) {
+            // call wantToSkip
+            console.log("// call wantToSkip");
+        }else if ((key === "ArrowRight" && preKey[0] === "KeyA")){
+            // call wantToSkip
+            console.log("// call wantToSkip");       
+        }else if ((key === "KeyA" && preKey[0] === "KeyA")){
+            // call answerAlreadyDisplayed
+            console.log("// call answerAlreadyDisplayed"); 
+        }else if ((key === "KeyA" && preKey[0] === "KeyY")){
+            // call answerShowingAndScored, will need arguement of score
+            console.log("// call answerShowingAndScored -  Y branch"); 
+        }else if ((key === "KeyA" && preKey[0] === "KeyN")){
+            // call answerShowingAndScored, will need arguement of score
+            console.log("// call answerShowingAndScored - N branch"); 
+        }else if ((key === "KeyY" && preKey[0] === "ArrowRight")){
+            // call youHaveNotSeenAnswer
+            console.log("// call youHaveNotSeenAnswer - from Y");
+        }else if ((key === "KeyY" && preKey[0] === "KeyY")){
+            // call alreadyScored
+            console.log("// call alreadyScored - Y");
+        }else if ((key === "KeyY" && preKey[0] === "KeyN")){
+            // call alreadyScored
+            console.log("// call alreadyScored - N");  
+        }else if ((key === "KeyN" && preKey[0] === "ArrowRight")){
+            // call youHaveNotSeenAnswer
+            console.log("// call youHaveNotSeenAnswer - from N branch");
+        }else if ((key === "KeyN" && preKey[0] === "KeyY")){
+            // call alreadyScored
+            console.log("// call alreadyScored - Y");
+        }else if ((key === "KeyN" && preKey[0] === "KeyN")){
+            // call alreadyScored
+            console.log("// call alreadyScored - N");  
+        }else {
+            console.log("You fell through invalidKeyPressed.  You should NOT be here!!!")
+        }
+    };
+};
 
 
 // https://developer.mozilla.org/en-US/docs/Web/API/Document/keydown_event for logkey syntax
@@ -80,12 +120,15 @@ function logKey(event) {
     console.log(`${keyPushed}`);
     if (keyPushed === "ArrowRight" && previousKeysPressed[0] === "START") {
         // call nextCard to START
+        nextCard();
         console.log("// call nextCard at START");   
     }else if (keyPushed === "ArrowRight" && (previousKeysPressed[0] === "KeyY")){
         // call nextCard after 'Y', separate from KeyN due to falsey issue with ||
+        nextCard ();
         console.log("// call nextCard with previous of KeyY");
     }else if (keyPushed === "ArrowRight" && (previousKeysPressed[0] === "KeyN")){
         // call nextCard after 'N'
+        nextCard();
         console.log("// call nextCard with previous of KeyN");
     }else if (keyPushed === "KeyA" && previousKeysPressed[0] === "ArrowRight"){
         // call showAnswer to flip card to show the answer
@@ -108,6 +151,12 @@ function logKey(event) {
 };
 // document.querySelector(".cardFront", "#5").innerText = keyPushed;
 // newP.innerText = defaultQuote.author;   //? maybe author.value
-document.addEventListener('keydown', logKey)
-console.log(cardFront.innerText);
+
+document.addEventListener('keydown', logKey);
+
+    
+// need to add checkWrongStack
+
+// document.addEventListener('keydown', logKey)
+// console.log(cardFront.innerText);
 console.log("prettier");
