7ed548d1c99279259c5febfd69de75ac9645dcbb
Darryl Grissom <GrissomDarrylE@JohnDeere>
2020-07-13 14:01:52 -0500

    exploded some ELSE IF's due to falsey with ||

--- script.js
+++ script.js
@@ -17,7 +17,10 @@ const flashCards = [
         quote:"The pessimist sees difficulty in every opportunity. The optimist sees opportunity in every difficulty.",
         author:"Winston Churchill"
     }];
-console.log(flashCards[1].prompt)
+console.log(Array.isArray(flashCards));
+console.log(flashCards[0].prompt);
+console.log(flashCards[0].quote);
+
 
 // flashCards.forEach(card => {
 
@@ -25,43 +28,46 @@ console.log(flashCards[1].prompt)
 
 function invalidKeyPressed(key, preKey){
     if (key !== ("ArrowRight" || "KeyA" || "KeyY" || "KeyN")){
+        console.log("key argument - " + key);
         console.log("Not a game key, // call showHelpKeys");
         // call showHelpKeys
-    }else if ((keyPushed === "ArrowRight" && previousKeysPressed[0] === "ArrowRight")) {
+    }else if ((key === "ArrowRight" && preKey[0] === "ArrowRight")) {
         // call wantToSkip
         console.log("// call wantToSkip");
-    }else if ((keyPushed === "ArrowRight" && previousKeysPressed[0] === "KeyA")){
+    }else if ((key === "ArrowRight" && preKey[0] === "KeyA")){
          // call wantToSkip
          console.log("// call wantToSkip");       
-    }else if ((keyPushed === "KeyA" && previousKeysPressed[0] === "KeyA")){
+    }else if ((key === "KeyA" && preKey[0] === "KeyA")){
         // call answerAlreadyDisplayed
         console.log("// call answerAlreadyDisplayed"); 
-    }else if ((keyPushed === "KeyA" && previousKeysPressed[0] === "KeyY")){
+    }else if ((key === "KeyA" && preKey[0] === "KeyY")){
         // call answerShowingAndScored, will need arguement of score
         console.log("// call answerShowingAndScored -  Y branch"); 
-    }else if ((keyPushed === "KeyA" && previousKeysPressed[0] === "KeyN")){
+    }else if ((key === "KeyA" && preKey[0] === "KeyN")){
         // call answerShowingAndScored, will need arguement of score
         console.log("// call answerShowingAndScored - N branch"); 
-    }else if ((keyPushed === "KeyY" && previousKeysPressed[0] === "ArrowRight")){
+    }else if ((key === "KeyY" && preKey[0] === "ArrowRight")){
         // call youHaveNotSeenAnswer
         console.log("// call youHaveNotSeenAnswer - from Y");
-    }else if ((keyPushed === "KeyY" && previousKeysPressed[0] === "KeyY")){
+    }else if ((key === "KeyY" && preKey[0] === "KeyY")){
         // call alreadyScored
         console.log("// call alreadyScored - Y");
-    }else if ((keyPushed === "KeyY" && previousKeysPressed[0] === "KeyN")){
+    }else if ((key === "KeyY" && preKey[0] === "KeyN")){
         // call alreadyScored
         console.log("// call alreadyScored - N");  
-    }else if ((keyPushed === "KeyN" && previousKeysPressed[0] === "ArrowRight")){
+    }else if ((key === "KeyN" && preKey[0] === "ArrowRight")){
         // call youHaveNotSeenAnswer
         console.log("// call youHaveNotSeenAnswer - from N branch");
-    }else if ((keyPushed === "KeyN" && previousKeysPressed[0] === "KeyY")){
+    }else if ((key === "KeyN" && preKey[0] === "KeyY")){
         // call alreadyScored
         console.log("// call alreadyScored - Y");
-    }else if ((keyPushed === "KeyN" && previousKeysPressed[0] === "KeyN")){
+    }else if ((key === "KeyN" && preKey[0] === "KeyN")){
         // call alreadyScored
         console.log("// call alreadyScored - N");  
-    }
-}
+    }else {
+        console.log("You fell through invalidKeyPressed.  You should NOT be here!!!")
+    };
+};
 
 // console.log("in ELSE of f invalidKeyPressed");
 
@@ -75,9 +81,12 @@ function logKey(event) {
     if (keyPushed === "ArrowRight" && previousKeysPressed[0] === "START") {
         // call nextCard to START
         console.log("// call nextCard at START");   
-    }else if (keyPushed === "ArrowRight" && (previousKeysPressed[0] === ("KeyY" || "KeyN"))){
-        // call nextCard after 'Y' or 'N'
-        console.log("// call nextCard with previous of KeyY || KeyN");
+    }else if (keyPushed === "ArrowRight" && (previousKeysPressed[0] === "KeyY")){
+        // call nextCard after 'Y', separate from KeyN due to falsey issue with ||
+        console.log("// call nextCard with previous of KeyY");
+    }else if (keyPushed === "ArrowRight" && (previousKeysPressed[0] === "KeyN")){
+        // call nextCard after 'N'
+        console.log("// call nextCard with previous of KeyN");
     }else if (keyPushed === "KeyA" && previousKeysPressed[0] === "ArrowRight"){
         // call showAnswer to flip card to show the answer
         console.log("// call showAnswer to flip card and show the answer");
